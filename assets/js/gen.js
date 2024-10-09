b916c44744b3ef03fef2b1b0360a561ba7a885ae
Yael Massieu <88387946+yaelsito@users.noreply.github.com>
2024-10-09 14:38:11 -0600

    v2-0
    renamed vars
    updated code

--- assets/js/gen.js
+++ assets/js/gen.js
@@ -1,103 +1,107 @@
 /*
-reNamso v1.5 is th new and modern Namso CCGEN.
+reNamso v2.0 is th new and modern Namso CCGEN.
 RE-WRITED BY YAEL MASSIEU.
-Telegram: @yaelmassieu
+Instagram: @is.leay
 Portfolio: https://yael.pages.dev/
 */
 document.addEventListener('DOMContentLoaded', () => {
-  setYear()
-  document.getElementById('generar').addEventListener('click', function () {
-    const tr = 100
-    const binInputUsr = document.getElementById('ccpN').value
-    darkside(binInputUsr, tr)
-  })
-  document.getElementById('ccpN').addEventListener('change', fillter)
-  document.getElementById('cleanText').addEventListener('click', cleanText)
-})
-function fillter() {
-  const bin = document.getElementById('ccpN').value
-  let maxInputCC
-  const ccBrand = chkCard(bin)
-  switch (ccBrand) {
+  showCurrentYear();
+  document.getElementById('generar').addEventListener('click', ()=> {
+    const maxCards = 100;
+    const binInput = document.getElementById('ccpN').value;
+    generateCardNumbers(binInput, maxCards);
+  });
+  document.getElementById('ccpN').addEventListener('change', addXToBin);
+  document.getElementById('cleanText').addEventListener('click', resetTextarea);
+});
+
+function addXToBin() {
+  const bin = document.getElementById('ccpN').value;
+  let maxLengthCard;
+  const cardBrand = getCardBrand(bin);
+  switch (cardBrand) {
     case 'American Express':
-      maxInputCC = 15
-      break
+      maxLengthCard = 15;
+      break;
     case 'Diners Club':
-      maxInputCC = 14
-      break
+      maxLengthCard = 14;
+      break;
     default:
-      maxInputCC = 16
-      break
+      maxLengthCard = 16;
+      break;
   }
-  const getUserBin = bin.padEnd(maxInputCC, 'x')
-  document.getElementById('ccpN').value = getUserBin
+  const newBin = bin.padEnd(maxLengthCard, 'x');
+  document.getElementById('ccpN').value = newBin;
 }
-const rnd = (frN, toN) => Math.floor(Math.random() * (toN - frN + 1)) + frN
-const unilenS = (aS, ul, fc = '0', p = 0) => {
-  let rS = '' + aS
-  ul = Number.parseFloat(ul)
-  if (!fc || !p) {
-    fc = '0'
-    p = 0
+
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const format = (input, length, fillChar = '0', padding = 0) => {
+  let result = '' + input;
+  length = Number.parseFloat(length);
+  if (!fillChar || !padding) {
+    fillChar = '0';
+    padding = 0;
   }
-  const rL = rS.length
-  if (rL < ul) {
-    rS = p === 0 ? fc.repeat(ul - rL) + rS : rS + fc.repeat(ul - rL)
+  const actuaLength = result.length;
+  if (actuaLength < length) {
+    result = padding === 0 ? fillChar.repeat(length - actuaLength) + result : result + fillChar.repeat(length - actuaLength);
   }
-  return rS
-}
-function darkside(p1, tr) {
-  tr = tr || 1
-  const ccghm = Math.min(Math.max(parseInt(document.getElementById('ccghm').value), 1), 100)
-  let out = ''
-  if (p1) {
-    const selectedOption = document.getElementById('ccoudatfmt').options[document.getElementById('ccoudatfmt').selectedIndex]
-    const formatPrefix = (selectedOption.value === 'xml') ? '<xml>\n' : (selectedOption.value === 'json') ? '{\n' : ''
-    let clcd, ccck, cdi
-    for (let k = 1; k <= ccghm; k++) {
-      const p = p1
-      const cn = chkCard(p1)
-      clcd = false
-      ccck = false
-      cdi = ''
-      for (let i = tr; i >= 1; i--) {
-        document.getElementById('output2').value = 'Generating...'
-        cdi = sbtStringSpRnd(p, 'x', '0123456789')
-        const cf = sbtString(cdi, ' -/abcdefghijklmnopqrstuvwyzABCDEFGHIJLMNOPQRSTUVWYZ')
-        clcd = chkLCD(cf)
-        ccck = chkCCCksum(cf, cn)
-        if (clcd && ccck) break
+  return result;
+};
+
+function generateCardNumbers(cardBin, maxCards) {
+  maxCards = maxCards || 1;
+  const limitOfCards = Math.min(Math.max(parseInt(document.getElementById('ccghm').value), 1), 100);
+  let result = '';
+  if (cardBin) {
+    const formatOfGen = '';
+    let isGenerated, isValidC, generatedCard;
+    for (let i = 1; i <= limitOfCards; i++) {
+      const binCard = cardBin;
+      const checkCardBrand = getCardBrand(cardBin);
+      isGenerated = false;
+      isValidC = false;
+      generatedCard = '';
+      for (let i = maxCards; i >= 1; i--) {
+        document.getElementById('output2').value = 'Generating...';
+        generatedCard = randomSustitute(binCard, 'x', '0123456789');
+        const formattedCardNumber = deleteInvalidShits(generatedCard, ' -/abcdefghijklmnopqrstuvwyzABCDEFGHIJLMNOPQRSTUVWYZ');
+        isGenerated = isValidLuhn(formattedCardNumber);
+        isValidC = isValidChecksum(formattedCardNumber, checkCardBrand);
+        if (isGenerated && isValidC) break;
       }
-      if (clcd && ccck) {
-        const j = document.getElementById('ccnsp').selectedIndex
-        const ccnspc = (j === 1) ? ' ' : (j === 2) ? '-' : ''
-        let cdif = ''
-        for (let i = 0; i < cdi.length; i++) {
-          const aS = (cdi[i] === ' ') ? ccnspc : cdi[i]
-          cdif += aS
+      if (isGenerated && isValidC) {
+        const selectedSeparatorIndex = document.getElementById('ccnsp').selectedIndex;
+        const separator = (selectedSeparatorIndex === 1) ? ' ' : (selectedSeparatorIndex === 2) ? '-' : '';
+        let formattedNumber = '';
+        for (let i = 0; i < generatedCard.length; i++) {
+          const char = (generatedCard[i] === ' ') ? separator : generatedCard[i];
+          formattedNumber += char;
         }
-        let expDate = ''
+        let expDate = '';
         if (document.getElementById('ccexpdat').checked) {
-          const dnowi = new Date()
-          const mes = (document.getElementById('emeses').value === 'rnd') ? unilenS(rnd(1, 12), 2, '0', 0) : document.getElementById('emeses').value
-          const year = (document.getElementById('eyear').value === 'rnd') ? (dnowi.getFullYear() + rnd(2, 6)) : document.getElementById('eyear').value
-          expDate = `|${mes}|${year}`
+          const currentDate = new Date();
+          const month = (document.getElementById('emeses').value === 'rnd') ? format(randomNum(1, 12), 2, '0', 0) : document.getElementById('emeses').value;
+          const year = (document.getElementById('eyear').value === 'rnd') ? (currentDate.getFullYear() + randomNum(2, 6)) : document.getElementById('eyear').value;
+          expDate = `|${month}|${year}`;
         }
-        const eccv = (document.getElementById('eccv').value === 'rnd' && document.getElementById('ccvi').checked)
-          ? ((cdif.startsWith('34') || cdif.startsWith('37'))
+        const randomCVV = (document.getElementById('eccv').value === 'rnd' && document.getElementById('ccvi').checked)
+          ? ((formattedNumber.startsWith('34') || formattedNumber.startsWith('37'))
             ? (Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000)
             : (Math.floor(Math.random() * (998 - 112 + 1)) + 112))
-          : document.getElementById('eccv').value
-        out += `${formatPrefix}${cdif}${expDate}${(document.getElementById('ccvi').checked) ? ('|' + eccv) : ''}${(document.getElementById('ccbank').checked) ? ('|' + cn) : ''}\n`
+          : document.getElementById('eccv').value;
+        result += `${formatOfGen}${formattedNumber}${expDate}${(document.getElementById('ccvi').checked) ? ('|' + randomCVV) : ''}${(document.getElementById('ccbank').checked) ? ('|' + checkCardBrand) : ''}\n`;
       } else {
-        out = 'Error'
-        break
+        result = 'Error';
+        break;
       }
     }
-    document.getElementById('output2').value = out
+    document.getElementById('output2').value = result;
   }
 }
-function chkCard(cdi) {
+
+function getCardBrand(cardNumber) {
   const brands = [
     { pattern: /^4/, name: 'Visa' },
     { pattern: /^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01])/i, name: 'Mastercard' },
@@ -105,62 +109,65 @@ function chkCard(cdi) {
     { pattern: /^(34|37)/, name: 'American Express' },
     { pattern: /^(30[0-5]|309|36|38|39)/, name: 'Diners Club' },
     { pattern: /^35(2[89]|[3-8][0-9])/, name: 'JCB' }
-  ]
-  cdi = sbtString(cdi, ' -/abcdefghijklmnopqrstuvwyzABCDEFGHIJLMNOPQRSTUVWYZ')
+  ];
+  cardNumber = deleteInvalidShits(cardNumber, ' -/abcdefghijklmnopqrstuvwyzABCDEFGHIJLMNOPQRSTUVWYZ');
   for (let i = 0; i < brands.length; i++) {
-    if (cdi.match(brands[i].pattern)) {
-      return brands[i].name
+    if (cardNumber.match(brands[i].pattern)) {
+      return brands[i].name;
     }
   }
-  return 'Unknown'
+  return 'Unknown';
 }
-function chkCCCksum(cf, cn) {
-  const w = [2, 1]
-  let ml = 0
-  let j = 0
-  for (let i = cf.length - 2; i >= 0; i--) {
-    const m = parseInt(cf[i], 10) * w[j % w.length]
-    ml += Math.floor(m / 10) + (m % 10)
-    j++
+
+function isValidChecksum(cardNumber) {
+  const weights = [2, 1];
+  let sum = 0;
+  let index = 0;
+  for (let i = cardNumber.length - 2; i >= 0; i--) {
+    const result = parseInt(cardNumber[i], 10) * weights[index % weights.length];
+    sum += Math.floor(result / 10) + (result % 10);
+    index++;
   }
-  const ml2 = 10 - (ml % 10)
-  const ml1 = (10 - ((ml + ml2) % 10)) % 10
-  return parseInt(cf[cf.length - 1], 10) === ml1 ? ml1 : parseInt(cf[cf.length - 1], 10)
+  const remainder = 10 - (sum % 10);
+  const checksum = (10 - ((sum + remainder) % 10)) % 10;
+  return parseInt(cardNumber[cardNumber.length - 1], 10) === checksum ? checksum : parseInt(cardNumber[cardNumber.length - 1], 10);
 }
-function chkLCD(cf) {
-  let ml = 0
-  let j = false
-  for (let i = cf.length - 1; i >= 0; i--) {
-    let m = parseInt(cf[i], 10)
-    if (j) {
-      m *= 2
-      if (m > 9) {
-        m -= 9
+
+function isValidLuhn(cardNumber) {
+  let sum = 0;
+  let alternate = false;
+  for (let i = cardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(cardNumber[i], 10);
+    if (alternate) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
       }
     }
-    ml += m
-    j = !j
+    sum += digit;
+    alternate = !alternate;
   }
-  return (ml % 10) === 0
+  return (sum % 10) === 0;
 }
-function sbtString(s1, s2) {
-  let ous = ''
-  for (let i = 0; i < s1.length; i++) {
-    const c1 = s1.charAt(i)
-    const c2 = s2.indexOf(c1)
-    if (c2 === -1) ous += c1
+
+function deleteInvalidShits(input, invalidChars) {
+  let result = '';
+  for (let i = 0; i < input.length; i++) {
+    const justInputs = input.charAt(i);
+    if (invalidChars.indexOf(justInputs) === -1) result += justInputs;
   }
-  return ous
+  return result;
 }
-function sbtStringSpRnd(s1, s2, bS = '0123456789') {
-  let ous = ''
-  for (let i = 0; i < s1.length; i++) {
-    const c1 = s1.charAt(i)
-    const c2 = s2.indexOf(c1)
-    if (c2 === -1) ous += c1
-    else ous += bS.charAt(Math.floor(Math.random() * bS.length))
+
+function randomSustitute(input, charsToReplace, replacementChars = '0123456789') {
+  let result = '';
+  for (let i = 0; i < input.length; i++) {
+    const justInputs = input.charAt(i);
+    if (charsToReplace.indexOf(justInputs) === -1) result += justInputs;
+    else result += replacementChars.charAt(Math.floor(Math.random() * replacementChars.length));
   }
-  return ous
+  return result;
 }
-const cleanText = () => document.querySelector('#output2').value = ''
-const setYear = () => document.querySelector('#date').textContent = new Date().getFullYear()
+
+const resetTextarea = () => document.querySelector('#output2').value = '';
+const showCurrentYear = () => document.querySelector('#date').textContent = new Date().getFullYear();
